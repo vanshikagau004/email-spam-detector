e731ef7bb9bc3bb6b2eeb9bc3bf0d779c97edff4
Vanshika Gautam <vanshikagautam004@gmail.com>
2026-03-11 15:13:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pickle
+
+st.title("Email Spam Detector")
+
+st.write("Enter a message to check if it is Spam or Not Spam.")
+
+model = pickle.load(open("model/spam_model.pkl", "rb"))
+vectorizer = pickle.load(open("model/vectorizer.pkl", "rb"))
+
+message = st.text_area("Enter your message")
+
+if st.button("Predict"):
+
+    if message != "":
+        vector = vectorizer.transform([message])
+        result = model.predict(vector)
+
+        if result[0] == 1:
+            st.error("This message is SPAM")
+        else:
+            st.success("This message is NOT SPAM")
+
+    else:
+        st.warning("Please enter a message")
